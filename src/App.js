e6e67bbf816dda579250d25d4669117ef416848e
GabiMereles <61704204+GabiMereles@users.noreply.github.com>
2020-03-21 19:19:17 -0300

    Update App.js
    Se agrega la S al http

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   
   useEffect(() => {
     var consultarApi=async() =>{
-      var url = `http://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=e6ec70ad3f4244cab5bc13271f6d44b1`;
+      var url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=e6ec70ad3f4244cab5bc13271f6d44b1`;
       var respuesta = await fetch (url);
       var noticias = await respuesta.json();
 
